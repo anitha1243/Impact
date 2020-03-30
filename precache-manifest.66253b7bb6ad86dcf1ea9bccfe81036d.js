e2849c40082e9f16b256083ee0fc7ea1bd25f9ab
anitha1243 <kurian.anithajose90@gmail.com>
2020-03-30 22:52:30 +1100

    Updates

---
+++ precache-manifest.66253b7bb6ad86dcf1ea9bccfe81036d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51528fa01c3594965b008e87486c839d",
+    "url": "/yourreponame/index.html"
+  },
+  {
+    "revision": "bc5b2ad8ee4c102f2b31",
+    "url": "/yourreponame/static/css/2.9b46dffb.chunk.css"
+  },
+  {
+    "revision": "613494e8640eba832439",
+    "url": "/yourreponame/static/css/main.da9c1b84.chunk.css"
+  },
+  {
+    "revision": "bc5b2ad8ee4c102f2b31",
+    "url": "/yourreponame/static/js/2.bc23e64d.chunk.js"
+  },
+  {
+    "revision": "44eadc3c045b02392e4143dbbec91ff3",
+    "url": "/yourreponame/static/js/2.bc23e64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613494e8640eba832439",
+    "url": "/yourreponame/static/js/main.32af20cc.chunk.js"
+  },
+  {
+    "revision": "a0c2a96852e822a587b4",
+    "url": "/yourreponame/static/js/runtime-main.48560c9d.js"
+  },
+  {
+    "revision": "58fd93e440b9bc125fe26471e4c46e3f",
+    "url": "/yourreponame/static/media/background.58fd93e4.jpg"
+  },
+  {
+    "revision": "72469c246a3851f7f23a82098ad3a491",
+    "url": "/yourreponame/static/media/bendigoSponsor.72469c24.png"
+  },
+  {
+    "revision": "90979e140fcfe220f4d122eae80d92c7",
+    "url": "/yourreponame/static/media/exit.90979e14.png"
+  },
+  {
+    "revision": "cc6ac04c12ddc52fd81dee04237ece48",
+    "url": "/yourreponame/static/media/impactLogo.cc6ac04c.png"
+  },
+  {
+    "revision": "f98bce028d0701d1d96dec8e6f11a5e1",
+    "url": "/yourreponame/static/media/impactPoster.f98bce02.png"
+  },
+  {
+    "revision": "b4c5a85c95e3b01b0a88c5b167bcaac1",
+    "url": "/yourreponame/static/media/map.b4c5a85c.PNG"
+  },
+  {
+    "revision": "4af8e4a1cb6f6b1e891da7f6268fabae",
+    "url": "/yourreponame/static/media/policeSponsor.4af8e4a1.png"
+  },
+  {
+    "revision": "58b12c1c81f1f01f99542bbde7b39a41",
+    "url": "/yourreponame/static/media/registeredCharity.58b12c1c.png"
+  }
+]);
